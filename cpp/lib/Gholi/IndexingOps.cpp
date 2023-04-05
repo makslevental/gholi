16924e193c7fb7adbb6a384f370ebdb6c42ea200
mlevental <maksim.levental@gmail.com>
2023-04-04 22:53:55 -0500

    gatherop wip

--- cpp/lib/Gholi/IndexingOps.cpp
+++ cpp/lib/Gholi/IndexingOps.cpp
@@ -8,7 +8,29 @@
 
 #include "Gholi/IndexingOps.h"
 #include "Gholi/IndexingDialect.h"
+#include "mlir/Dialect/Tensor/IR/Tensor.h"
 #include "mlir/IR/OpImplementation.h"
+#include "mlir/Support/LogicalResult.h"
 
 #define GET_OP_CLASSES
 #include "Gholi/IndexingOps.cpp.inc"
+
+using namespace mlir;
+using namespace mlir::tensor;
+
+LogicalResult mlir::gholi::GatherOp::inferReturnTypes(
+    MLIRContext *context, std::optional<Location> location, ValueRange operands,
+    DictionaryAttr attributes, RegionRange regions,
+    SmallVectorImpl<Type> &inferredReturnTypes) {
+
+  ArrayRef<int64_t> coordinates =
+      attributes.get("coordinates").cast<mlir::DenseI64ArrayAttr>();
+  RankedTensorType expectedResultType = mlir::tensor::GatherOp::inferResultType(
+      // source
+      operands[0].getType().cast<RankedTensorType>(),
+      // indices
+      operands[1].getType().cast<RankedTensorType>(), coordinates,
+      /*rankReduced=*/true);
+  inferredReturnTypes.assign({expectedResultType});
+  return success();
+}
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,13 +1,17 @@
 from textwrap import dedent
 
+import numpy as np
+from nelli.mlir._mlir._mlir_libs._mlir.ir import Value
 from nelli.mlir.arith import constant
 from nelli.mlir.func import mlir_func
+from nelli.mlir.tensor import tensor_dialect as tensor
+
 from nelli.mlir.utils import I32, run_pipeline
 from nelli.utils import mlir_mod_ctx
 
+from gholi.mlir.dialects.indexing import GatherOp, CustomType, tensor_gather
 from gholi.mlir.passes import Pipeline
 from util import check_correct
-from gholi.mlir.dialects.indexing import FooOp, CustomType
 
 
 class TestIfs:
@@ -17,14 +21,19 @@ class TestIfs:
             @mlir_func
             def foo(x: CustomType.get("bob")):
                 cst = constant(2, type=I32)
-                f = FooOp(cst)
+                assert isinstance(cst, Value)
+                source = tensor.EmptyOp([10, 10], I32)
+                indices = tensor.EmptyOp([1, 2, 2], I32)
+                f = GatherOp(source, indices, [0, 1])
 
         correct = dedent(
             """\
         module {
           func.func @foo(%arg0: !indexing.custom<"bob">) {
             %c2_i32 = arith.constant 2 : i32
-            %0 = indexing.foo %c2_i32 : i32
+            %0 = tensor.empty() : tensor<10x10xi32>
+            %1 = tensor.empty() : tensor<1x2x2xi32>
+            %2 = indexing.gather %0[%1] coordinates = [0, 1] : (tensor<10x10xi32>, tensor<1x2x2xi32>) -> tensor<1x2xi32>
             return
           }
         }
@@ -42,3 +51,46 @@ class TestIfs:
         run_pipeline(module, Pipeline().indexing_switch_bar_foo().materialize())
 
         assert "func.func @foo()" in str(module)
+
+    def test_gather(self):
+        # %out = tensor.gather %source[%indices] gather_dims([0, 1, 2]) :
+        #   (tensor<4x4x4xf32>, tensor<1x2x 3xindex>) -> tensor<1x2x 1x1x1xf32>
+
+        source = np.random.randint(0, 100, (4, 4, 4))
+        indices = np.random.randint(0, 4, (1, 2, 3))
+        gather_dims = [0, 1, 2]
+
+        res = tensor_gather(source, indices, gather_dims)
+        assert res.shape == (1, 2)
+
+        #  for 6 rows of 7 columns take from source along dim 1 everything else
+        #  %out = tensor.gather %source[%indices] gather_dims([1]) :
+        #    (tensor<3x4x5xf32>, tensor<6x7x 1xindex>) -> tensor<6x7x 3x1x5xf32>
+
+        source = np.random.randint(0, 100, (3, 4, 5))
+        indices = np.random.randint(0, 4, (6, 7, 1))
+        gather_dims = [1]
+
+        res = tensor_gather(source, indices, gather_dims)
+        assert res.shape == (6, 7, 3, 5)
+
+        #   %gathered = tensor.gather %dest[%indices_i32] gather_dims([1, 2]) unique:
+        #     (tensor<4x5x6xf32>, tensor<1x3x2xi32>) -> tensor<1x3x4x1x1xf32>
+
+        source = np.random.randint(0, 100, (4, 5, 6))
+        indices = np.random.randint(0, 4, (1, 3, 2))
+        gather_dims = [1, 2]
+
+        res = tensor_gather(source, indices, gather_dims)
+        assert res.shape == (1, 3, 4)
+
+        # %0 = tensor.empty() : tensor<10x10xi32>
+        # %1 = tensor.empty() : tensor<1x2x2xi32>
+        # %2 = indexing.gather %0[%1] coordinates = [0, 1] : (tensor<10x10xi32>, tensor<1x2x2xi32>) -> tensor<1x2xi32>
+
+        source = np.random.randint(0, 100, (10, 10))
+        indices = np.random.randint(0, 4, (1, 2, 2))
+        gather_dims = [0, 1]
+
+        res = tensor_gather(source, indices, gather_dims)
+        assert res.shape == (1, 2)
